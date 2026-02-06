c913212c6de8043259d3947f28820a02c293cbd3
Rich <rchppls@gmail.com>
2026-02-06 15:30:57 -0700

    Completed Lab

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 //Write your code here
 
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
+// Log the name of the attendee
+function logAttendeeName(attendee) {
+  console.log(attendee.name)
+}
+// Log the ticket price of the attendee
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice)
+}
+// Update the ticket type of the attendee
+function updateTicketType(attendee, newType) {
+  attendee.ticketType = newType
+}
+// Update the ticket price of the attendee
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice
+}
+// Remove the event property from the attendee object
+function removeEventProperty(attendee) {
+  delete attendee.event
+}
+// Indicate if the attendee has checked in property
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true
+}
 
 
 //Needed for the tests to work. Don't modify
